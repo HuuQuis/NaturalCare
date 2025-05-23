85059030c53ef07dd25030b616eb91835fc422a4
Nguyen Lam Tung <tungnlhe180026@fpt.edu.vn>
2025-05-24 00:27:01 +0700

    Small fix + add DBContext

---
+++ src/main/java/dal/DBContext.java
@@ -0,0 +1,63 @@
+package dal;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author FPT University - PRJ30X
+ */
+public class DBContext {
+
+    protected Connection connection; //dung de ket noi den CSDL
+    protected PreparedStatement stm;//thuc hien cac cau lenh SQL
+    protected ResultSet rs;//dung de luu tru va xu li du lieu lay ve tu select
+    protected String sql; //luu tru cau lenh SQL
+
+    public DBContext() {
+        //@Students: You are allowed to edit user, pass, url variables to fit 
+        //your system configuration
+        //You can also add more methods for Database Interaction tasks. 
+        //But we recommend you to do it in another class
+        // For example : StudentDBContext extends DBContext , 
+        //where StudentDBContext is located in dal package, 
+        try {
+            String user = "root";
+            String pass = "1234";
+            String url = "jdbc:mysql://localhost:3306/natural_care?useSSL=false&serverTimezone=UTC";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public boolean testConnection() {
+        if (connection != null) {
+            try {
+                return !connection.isClosed();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+    //test connection
+        DBContext db = new DBContext();
+        if (db.testConnection()) {
+            System.out.println("Kết nối thành công!");
+        } else {
+            System.out.println("Kết nối thất bại!");
+        }
+    }
+}
