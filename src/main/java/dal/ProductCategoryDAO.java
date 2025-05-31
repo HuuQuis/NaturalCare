925bbbd6371a18021f593994af937aa343bb7134
linh <linhpthe173504@fpt.edu.vn>
2025-05-31 16:37:57 +0700

    Update feature category

--- src/main/java/dal/ProductCategoryDAO.java
+++ src/main/java/dal/ProductCategoryDAO.java
@@ -24,4 +24,58 @@ public class ProductCategoryDAO extends DBContext {
         }
         return list;
     }
+
+    public void addProductCategory(String name) {
+        sql = "INSERT INTO product_category (product_category_name) VALUES (?)";
+        try {
+            stm = connection.prepareStatement(sql);
+            stm.setString(1, name);
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateProductCategory(int id, String name) {
+        sql = "UPDATE product_category SET product_category_name = ? WHERE product_category_id = ?";
+        try {
+            stm = connection.prepareStatement(sql);
+            stm.setString(1, name);
+            stm.setInt(2, id);
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteProductCategory(int id) {
+        sql = "DELETE FROM product_category WHERE product_category_id = ?";
+        try {
+            stm = connection.prepareStatement(sql);
+            stm.setInt(1, id);
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ProductCategory getById(int id) {
+        sql = "SELECT * FROM product_category WHERE product_category_id = ?";
+        try {
+            stm = connection.prepareStatement(sql);
+            stm.setInt(1, id);
+            rs = stm.executeQuery();
+            if (rs.next()) {
+                return new ProductCategory(
+                        rs.getInt("product_category_id"),
+                        rs.getString("product_category_name")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
