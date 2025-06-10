af8d16b335943635df3ff9488730735b6e4fd15f
anhphe172475 <phahaianh2210@gmail.com>
2025-06-10 07:43:41 +0700

    [HaiAnh] Check already exist expert

--- src/main/java/dal/ExpertDAO.java
+++ src/main/java/dal/ExpertDAO.java
@@ -197,7 +197,6 @@ public class ExpertDAO extends DBContext {
                 psExpertSkill.setInt(2, skillId);
                 psExpertSkill.executeUpdate();
             }
-            
 
             connection.commit();
 
@@ -216,4 +215,20 @@ public class ExpertDAO extends DBContext {
             }
         }
     }
+    
+    public boolean isUsernameTakenForRole(String username, int roleId) {
+        String sql = "SELECT COUNT(*) FROM user WHERE username = ? AND role_id = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, username);
+            ps.setInt(2, roleId);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1) > 0;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
