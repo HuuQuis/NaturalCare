75700a488dc3559a18f21d1cd97904fa1a3dfda4
anhphe172475 <phahaianh2210@gmail.com>
2025-06-26 19:02:58 +0700

    Update user dao get all shipper

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -14,6 +14,18 @@ public class User {
     public User() {
     }
 
+    public User(int id, String username, String password, String firstName, String lastName, String email, String phone, int role, String avatar) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.role = role;
+        this.avatar = avatar;
+    }
+
     public int getId() {
         return id;
     }
